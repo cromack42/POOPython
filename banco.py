3052019a8709b792a2a7c00dacc503cab536555e
Marcelo Cromack <74430450+cromack42@users.noreply.github.com>
2024-04-17 20:51:06 -0300

    Update banco.py

--- banco.py
+++ banco.py
@@ -63,7 +63,7 @@ class BancoDedados:
                         FOREIGN KEY(cpf_proprietario) REFERENCES Pessoa(cpf),
                         FOREIGN KEY(id_marca) REFERENCES Marca(id))"""
                         )
-                        self.conn.commit()
+                self.conn.commit()
             except sqlite3.Error as e:
                 print(f"Erro ao criar tabela Veiculo: {e}")
 
@@ -73,7 +73,7 @@ class BancoDedados:
                 cursor = self.conn.cursor()
                 cursor.execute("INSERT INTO Pessoa VALUES (?, ?, ?, ?)",
                 (pessoa.cpf, pessoa.nome, pessoa.nascimento, pessoa.oculos),)
-                        self.conn.commit()
+                self.conn.commit()
             except sqlite3.Error as e:
                 print(f"Erro ao inserir pessoa: {e}")
     
@@ -90,18 +90,77 @@ class BancoDedados:
                         veiculo.marca.id,
                     ),
                 )
-                self.conn.commit()
-        except sqlite3.Error as e:
-            print(f"Erro ao inserir veículo: {e}")
+                self.conn.commit() 
+            except sqlite3.Error as e:
+                print(f"Erro ao inserir veículo: {e}")
 
     def atualizar_pessoa(self, pessoa):
-        if self, conn:
+        if self.conn:
             try:
                 cursor = self.conn.cursor()
                 cursor.execute(
                     "UPDATE Pessoa SET nome=?, nascimento=?, oculos=? WHERE cpf=?",
                     (pessoa.nome, pessoa.nascimento, pessoa.oculos, pessoa.cpf),
                       )
-                      self.conn.commit()
+                self.conn.commit()
             except sqlite3.Error as e:
                 print(f"Error ao atualizar pessoa: {e}")
+
+    def buscar_todas_pessoas(self):
+        pessoas = []
+        if self.conn: 
+            try:
+                cursor = self.conn.cursor()
+                cursor.excute("SELECT * FROM Pessoa")
+                for row in cursor.fetchall():
+                    cpf, nome, nascimento, oculos = row
+                    pessoas.append(Pessoa(cpf, nome, nascimento, oculos))
+            except sqlite3.Error as e:
+                print(f"Erro ao buscar pessoas: {e}")
+        return pessoas
+
+    def buscar_todos_veiculos(self):
+        Veiculo = []
+        if self.conn:
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute("SELECT * FROM Veiculo")
+                for row in cursor.fetchall():
+                    placa, cor, cpf_proprietario, id_marca = row
+                    proprietario = self.buscarpessoa_por_cpf(cpf_proprietario)
+                    marca = self.buscar_marca_por_id(id_marca)
+                    veiculos.append(veiculo(palca, cor, proprietario, marca))
+            except sqlite3.Error as e:
+                print(f"Erro ao buscar veículos: {e}")
+        return veiculos
+                
+    def buscar_pessoa_por_cpf(self, cpf: int):
+        if self.conn:   
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute("SELECT * FROM Pessoa WHERE cpf=?", (cpf,))
+                row = cursor.fetchone()
+                if row:
+                    cpf, nome, nascimento, oculos = row
+                    return Pessoa(cpf, nome, nascimento, oculos)
+            except sqlite3.Error as e:
+                print(f"Erro ao buscar pessoa por cpf: {e}")
+        return None
+    
+    def buscar_marca_por_id(self, id: int):
+        if self.conn:
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute("SELECT * FROM Pessoa WHERE id=?", (id,))
+                row = cursor.fetchone()
+                if row:
+                    id, nome, sigla = row
+                    return Marca(id, nome, sigla)
+            except sqlite3.Error as e:
+                print(f"Erro ao buscar pessoa por ID: {e}")
+        return None
+    
+    def fechar_conexao(self):
+        if self.conn:
+            self.conn.close()
+            self.conn = None
